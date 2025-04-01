6db7976901a11db09368ef1f752853514bc520e3
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-04-01 08:46:13 +0800

    test: add large test method

--- test_class.py
+++ test_class.py
@@ -17,6 +17,19 @@ class TestClass(unittest.TestCase):
             num_rows,
         )
 
+    def test_maze_create_cells_large(self):
+        num_cols = 20
+        num_rows = 20
+        m1: Maze = Maze(num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1.get_cells()),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1.get_cells()[0]),
+            num_rows,
+        )
+
 
 if __name__ == "__main__":
     _ = unittest.main()
